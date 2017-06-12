dccd29d551469dae9e924db3d3cd836eed22adf7
Willy Raedy <wraedy@gmail.com>
2017-06-12 14:31:54 -0500

    final version

--- bash.js
+++ bash.js
@@ -1,17 +1,26 @@
 // Output a prompt
 var commands = require('./commands.js');
 
-function done (output) {
-  process.stdout.write(output);
-  process.stdout.write('\nprompt > ');
+function done (output, args) {
+  if (args[0]) {
+    // pipe into next cmd
+    console.log('args in done: ', args);
+    commands[args[0][0].toLowerCase()]( args[0].slice(1), output, done);
+  } else {
+    process.stdout.write(output);
+    process.stdout.write('\nprompt > ');
+  }
 }
 
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
-  let args = cmd.split(' ');
-  commands[args[0].toLowerCase()](args.slice(1), done);
+  var cmdString = data.toString().trim(); // remove the newline
+  var cmdList = cmdString.split(/\s*\|\s*/g);
+  let args = cmdList.map(function (el) {
+   return el.split(' ');
+  });
+  commands[args[0][0].toLowerCase()]( args.slice(1), args[0].slice(1), done);
 });
 
--- commands.js
+++ commands.js
@@ -26,26 +26,58 @@ function ls(args, done) {
 	});
 }
 
-function cat(args, done) {
+function cat(stdin, args, done) {
+	console.log('START CAT');
 	let output = '';
-	args = args.map(function(ele , index) {
-		fs.readFile(ele, function(err,data) {
-			if (err) throw err;
-			output += data + '\n';
-			if(index === args.length - 1 ) {
-				done(output);
-			}
+	if (typeof args === 'string') {
+		output = args;
+		done(output, stdin);
+	} else {
+		args = args.map(function(ele , index) {
+			fs.readFile(ele, function(err,data) {
+				if (err) throw err;
+				output += data + '\n';
+				if (index === args.length - 1 ) {
+					done(output, stdin);
+				}
+			});
 		});
-	});
+	}
 }
 
-function head(path, done) {
-	fs.readFile(path[0], function(err, data) {
-		if (err) {throw err;}
-		data = data.toString();
-		data = data.split('\n').slice(0, 5).join('\n');
-		done(data);
-	});
+function grep(stdin, args, done) {
+	let match = args[0];
+	let path = args[1];
+	if (typeof args !== 'string') {
+		fs.readFile(path, function (err, data) {
+			if (err) throw err;
+			let output = data.toString();
+			output = output.split('\n').filter(function (line) {
+				return line.includes(match);
+			}).join('\n');
+			done(output, stdin);
+		});
+	} else {
+		match = stdin[0];
+		let output = args.split('\n').filter(function (line) {
+				return line.includes(match);
+		}).join('\n');
+		done(output, stdin.slice(1));
+	}
+}
+
+function head(stdin, path, done) {
+	if (typeof path === 'string') {
+		var output = path.split('\n').slice(0, 5).join('\n');
+		done(output, stdin);
+	} else {
+		fs.readFile(path[0], function(err, data) {
+			if (err) {throw err;}
+			data = data.toString();
+			data = data.split('\n').slice(0, 5).join('\n');
+			done(data, stdin);
+		});
+	}
 }
 
 function tail(path, done) {
@@ -117,3 +149,4 @@ module.exports.tail = tail;
 module.exports.sort = sort;
 module.exports.uniq = uniq;
 module.exports.curl = curl;
+module.exports.grep = grep;
